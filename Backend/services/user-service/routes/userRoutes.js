3ec6b141656f5bd80e47f405d219b7bf38cb3542
AbednegoTenge <abednegotenge180@gmail.com>
2025-07-13 13:53:09 +0000

    adding log in checker route

--- Backend/services/user-service/routes/userRoutes.js
+++ Backend/services/user-service/routes/userRoutes.js
@@ -5,10 +5,12 @@ import validateRequest from '../../../libs/validators/validateRequest.js';
 
 const router = express.Router();
 
+router.get('/', authController.home);
 router.get('/api/register', authController.registerForm);
 router.get('/api/login', authController.loginForm);
 router.post('/api/register', registerValidator, validateRequest, authController.register);
 router.post('/api/login', authController.login);
 router.post('/api/logout', authController.logout);
+router.get('/api/loggedin', authController.isLoggedIn);    
 
 export default router;  
