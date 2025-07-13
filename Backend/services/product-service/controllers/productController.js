038f6c886377ac5a71dd35e75eddc6ec35e381a6
AbednegoTenge <abednegotenge180@gmail.com>
2025-07-13 11:22:03 +0000

    uploading file to S3 bucket in product controller

--- Backend/services/product-service/controllers/productController.js
+++ Backend/services/product-service/controllers/productController.js
@@ -1,10 +1,16 @@
 import Product from '../models/product.js';
+import { uploadFileToS3 } from '../../../config/awsS3.js';
 
 
 const productController = {
     async create(req, res) {
         try {
-            const { name, description, price, category, stock, imageUrl } = req.body;
+            const { name, description, price, category, stock} = req.body;
+            let image = null;
+
+            if (req.file) {
+                image = await uploadFileToS3(req.file);
+            }
 
             const newProduct = await Product.create({
                 name,
@@ -12,7 +18,7 @@ const productController = {
                 price,
                 category,
                 stock,
-                imageUrl
+                image
             });
 
             return res.status(201).json({ message: 'Product added successfully', product: newProduct});
