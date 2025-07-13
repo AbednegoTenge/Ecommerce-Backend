b672e2534e61082db2e511febf1ad8e1549b67a0
AbednegoTenge <abednegotenge180@gmail.com>
2025-07-13 11:24:30 +0000

    parsing multer middleware into route

--- Backend/services/product-service/routes/productRoutes.js
+++ Backend/services/product-service/routes/productRoutes.js
@@ -3,9 +3,10 @@ import productController from '../controllers/productController.js';
 import productValidator from '../../../libs/validators/productValidator.js';
 import validateRequest from '../../../libs/validators/validateRequest.js';
 import authMiddleware from '../../../libs/auth/middleware.js';
+import { upload } from '../../../libs/auth/multer.js';
 
 const router = express.Router();
 
-router.post('/api/add-products', authMiddleware, productValidator, validateRequest, productController.create);
+router.post('/api/add-products', authMiddleware, upload.single('image'), productValidator, validateRequest, productController.create);
 
 export default router;
